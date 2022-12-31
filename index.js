6cdee2cdc59f50fa6e0201e1db7cac439e86a1e9
williamjones <willj836@gmail.com>
2022-12-31 14:27:14 +0000

    functioning start button, checkboxes and slow/ fast toggle

--- index.js
+++ index.js
@@ -1,30 +1,66 @@
-class Grid {
-  constructor() {
+class GameOfLife {
+  constructor(numOfRows, numOfColumns) {
     this.grid = null;
+    this.intervalID = null;
+    this.speed = 200;
+    this.numOfRows = numOfRows;
+    this.numOfColumns = numOfColumns;
 
     const randomiseBtn = document.querySelector("#randomise");
     const clearBtn = document.querySelector("#clear");
+    const startBtn = document.querySelector("#start");
+    const fast = document.querySelector("#fast");
+    const slow = document.querySelector("#slow");
 
     document.addEventListener("DOMContentLoaded", () => {
       clearBtn.click();
-    })
+    });
+
+    startBtn.addEventListener("click", () => {
+      if (this.intervalID) {
+        clearInterval(this.intervalID);
+        this.intervalID = null;
+      } else {
+        this.start();
+      }
+    });
 
     randomiseBtn.addEventListener("click", () => {
       this.randomise();
       this.view();
-    })
+      clearInterval(this.intervalID);
+      this.intervalID = null;
+    });
 
     clearBtn.addEventListener("click", () => {
       this.clear();
       this.view();
-    })
+      clearInterval(this.intervalID);
+      this.intervalID = null;
+    });
+
+    fast.addEventListener("change", () => {
+      if (fast.checked) {
+        clearInterval(this.intervalID)
+        this.speed = 20;
+        this.start()
+      }
+    });
+
+    slow.addEventListener("change", () => {
+      if (slow.checked) {
+        clearInterval(this.intervalID)
+        this.speed = 200;
+        this.start()
+      }
+    });
   }
 
   randomise() {
     let array = [];
-    for (let i = 0; i < 35; i++) {
+    for (let i = 0; i < this.numOfRows; i++) {
       array.push([]);
-      for (let j = 0; j < 68; j++) {
+      for (let j = 0; j < this.numOfColumns; j++) {
         array[i][j] = Math.random() > 0.7 ? 1 : 0;
       }
     }
@@ -33,13 +69,17 @@ class Grid {
 
   clear() {
     let array = [];
-    for (let i = 0; i < 35; i++) {
+    for (let i = 0; i < this.numOfRows; i++) {
       array.push([]);
-      for (let j = 0; j < 68; j++) {
+      for (let j = 0; j < this.numOfColumns; j++) {
         array[i][j] = 0;
       }
     }
     this.grid = array;
+  }
+
+  start() {
+    this.intervalID = setInterval(() => this.nextGeneration(), this.speed);
   }
 
   view() {
@@ -48,10 +88,10 @@ class Grid {
       table = document.createElement("table");
       document.querySelector("#grid").append(table);
     }
-    table.innerHTML = '';
-    for (let i = 0; i < 35; i++) {
+    table.innerHTML = "";
+    for (let i = 0; i < this.numOfRows; i++) {
       let row = document.createElement("tr");
-      for (let j = 0; j < 68; j++) {
+      for (let j = 0; j < this.numOfColumns; j++) {
         let cell = document.createElement("td");
         let checkbox = document.createElement("input");
         checkbox.type = "checkbox";
@@ -60,13 +100,59 @@ class Grid {
         } else {
           checkbox.checked = false;
         }
+        checkbox.addEventListener("change", () => {
+          this.grid[i][j] = checkbox.checked ? 1 : 0;
+        });
         cell.append(checkbox);
         row.append(cell);
       }
       table.append(row);
     }
   }
-  
+
+  nextGeneration() {
+    let nextGen = [];
+    for (let i = 0; i < this.numOfRows; i++) {
+      nextGen.push([]);
+      for (let j = 0; j < this.numOfColumns; j++) {
+        let aliveNeighbours = this.countAliveNeighbours(i, j);
+        if (this.isCellAlive(i, j)) {
+          if (aliveNeighbours === 2 || aliveNeighbours === 3) {
+            nextGen[i][j] = 1;
+          } else {
+            nextGen[i][j] = 0;
+          }
+        } else {
+          if (aliveNeighbours === 3) {
+            nextGen[i][j] = 1;
+          } else {
+            nextGen[i][j] = 0;
+          }
+        }
+      }
+    }
+    this.grid = nextGen;
+    this.view();
+  }
+
+  countAliveNeighbours(row, column) {
+    let count = 0;
+    for (let i = row - 1; i <= row + 1; i++) {
+      for (let j = column - 1; j <= column + 1; j++) {
+        if (i === row && j === column) {
+          continue;
+        }
+        if (this.isCellAlive((i + this.numOfRows) % this.numOfRows, (j + this.numOfColumns) % this.numOfColumns)) {
+          count++;
+        }
+      }
+    }
+    return count;
+  }
+
+  isCellAlive(row, column) {
+    return this.grid[row][column] === 1;
+  }
 }
 
-let grid = new Grid();
+const game = new GameOfLife(35, 65);
